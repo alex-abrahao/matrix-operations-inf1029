35c90fe28f4935074c2c245a210de7f3444366e2
Alexandre Abrahão <alexandre1997pf@hotmail.com>
2021-12-01 00:00:13 -0300

    Ajusta script de compilação

--- T5/compile.sh
+++ T5/compile.sh
@@ -1,2 +1,3 @@
 echo "Compiling static"
-gcc -o matrix_lib_test matrix_lib_test.c matriix_lib_vh.c timer.c -I/opt/nec/ve/veos/include -L/opt/nec/ve/veoslib64 -Wl,-rpath=/opt/nec/ve/veos/lib64 -lveo
+gcc -o matrix_lib_test matrix_lib_test.c matrix_lib_vh.c timer.c -I/opt/nec/ve/veos/include -L/opt/nec/ve/veos/lib64 -Wl,-rpath=/opt/nec/ve/veos/lib64 -lveo
+echo "Done static"
